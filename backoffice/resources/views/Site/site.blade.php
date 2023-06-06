1a4afe927117ff411f0c08f0e2963512bf31931b
Alexandre Osovski <74330383+AlexandreOsovski@users.noreply.github.com>
2023-06-06 08:47:29 -0300

    adicionando site

--- backoffice/resources/views/Site/site.blade.php
+++ backoffice/resources/views/Site/site.blade.php
@@ -1,8 +1,26 @@
-@extends("../layouts/layout")
+<!DOCTYPE html>
+<html lang="pt-br">
+    @include('includes.header')
 
-@include('includes.navbar')
+<body>
 
-@section("content")
+    @include('includes.navbar_site')
+
+    <div class="container-fluid p-0">
+        <div class="">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="w-100" src="img/fundo_site.svg" alt="Image">
+                    <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
+
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
-@endsection
+
+</body>
+</html>
+
---
+++ backoffice/resources/views/includes/navbar_site.blade.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-expand-sm fixed-top navbar-light">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <img src="img/logo_site.png">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar1">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbar1">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link text-white" href="#">SOBRE</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">COMO COMEÇAR?</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link text-white" href="#">TIME</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link text-white" href="#">CONTATOS</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <button class="button-60" role="button"><a class="nav-link text-white" href="#">ÁREA DO ALUNO</a></button>
+                </li>
+            </ul>
+
+        </div>
+    </div>
+</nav>
+<div class="sticky-top bg-white hidden-spacer"> </div>
